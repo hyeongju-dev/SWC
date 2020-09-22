3ac94d3d58cdb0215d99080329f5c3376a305cca
hyeongju-dev <xxox71@gmail.com>
2020-09-22 20:09:53 +0900

    changed the way to solve this problem

--- 경사로.cpp
+++ 경사로.cpp
@@ -1,42 +1,100 @@
-// TODO: another way
-
+// It's more general way to solve a problem!
 #include <iostream>
+#define abs(a) (a>0? a : -a)
 using namespace std;
 
-int n, l, ret = 0;
-int map[200][100];
+int n, l;
+int map[100][100];
+
+int solve() {
+  int ans = 0;
+
+  for (int i=0; i<n; ++i) {
+    /* row */
+    int cnt = 1;  // length of flat road until now
+    bool passable = true; // can traverse the road(row or column) or not
+    for (int j=0; j<n-1; ++j) {
+      int diff = map[i][j] - map[i][j+1];
+
+      if (diff == -1) { // go higher
+        if (cnt < l) {
+          passable = false;
+          break;
+        } else {
+          cnt = 1;
+        }
+      } else if (diff == 1) { // go lower
+        cnt = 1;
+        j++;
+        while (j < n-1 && map[i][j] == map[i][j+1]) {
+          cnt++;
+          j++;
+        }
+        if (cnt < l) {
+          passable = false;
+          break;
+        } else {
+          cnt = cnt - l;
+          j--;
+        }
+      } else if (abs(diff) > 1) {
+        passable = false;
+        break;
+      } else {
+        cnt++;
+      }
+    }
+    if (passable)
+      ans++;
+
+    /* column */
+    cnt = 1;          // ^_^; encountered an error: redeclaration of 'int cnt'
+    passable = true;
+    for (int j=0; j<n-1; ++j) {
+      int diff = map[j][i] - map[j+1][i];
+
+      if (diff == -1) {
+        if (cnt < l) {
+          passable = false;
+          break;
+        } else {
+          cnt = 1;
+        }
+      } else if (diff == 1) {
+        cnt = 1;
+        j++;
+        while (j < n-1 && map[j][i] == map[j+1][i]) {
+          cnt++;
+          j++;
+        }
+        if (cnt < l) {
+          passable = false;
+          break;
+        } else {
+          cnt = cnt - l;
+          j--;
+        }
+      } else if (abs(diff) > 1) {
+        passable = false;
+        break;
+      } else {
+        cnt++;
+      }
+    }
+    if (passable)
+      ans++;
+  }
+
+  return ans;
+}
 
 int main() {
-    cin >> n >> l;
-    for (int y = 0; y < n; ++y)
-        for (int x = 0; x < n; ++x)
-            cin >> map[y][x];
+  cin >> n >> l;
+  for (int y=0; y<n; ++y)
+    for (int x=0; x<n; ++x)
+      cin >> map[y][x];
 
-    for (int y = 0; y < n; ++y)
-        for (int x = 0; x < n; ++x)
-            map[n+y][x] = map[x][y];
+  cout << solve() << endl;
 
-    int count = 0;
-    int i, j;
-
-    ret = 0;
-    for (i = 0; i < 2*n; ++i) {
-        count = 1;
-        for (j = 0; j < n-1; ++j) {
-            if (map[i][j] == map[i][j + 1])
-                ++count;
-            else if (map[i][j] + 1 == map[i][j + 1] && count >= l)
-                count = 1;
-            else if (map[i][j] - 1 == map[i][j + 1] && count >= 0)
-                count = (1 - l);
-            else
-                break;
-        }
-        if (j == (n-1) && count >= 0)
-            ++ret;
-    }
-
-    cout << ret << endl;
-
-    return 0;
+  return 0;
 }
